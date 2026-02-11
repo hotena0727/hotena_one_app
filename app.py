44aec6ec3dbc763f0adaa47a573c4d6ec480169b
hotena0727 <kissitai0727@gmail.com>
2026-02-11 14:51:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -47,8 +47,8 @@ st.set_page_config(page_title="왕초보 탈출 하테나일본어", layout="cen
 
 BASE_DIR = Path(__file__).resolve().parent
 CSV_PATH = BASE_DIR / "data" / "words_beginner.csv"   # ✅ 왕초보 단어 CSV
-PATTERN_CSV_PATH = BASE_DIR / "data" / "required_patterns.csv"  # ✅ 필수패턴 CSV
-APP_URL = "https://YOUR_STREAMLIT_APP_URL_HERE/"      # ✅ 이메일 인증 redirect용 (스트림릿 앱 주소로 교체)
+PATTERN_CSV_PATH = BASE_DIR / "data" / "required_patterns1.csv"  # ✅ 필수패턴 CSV
+APP_URL = "https://hotenaoneapp-cvztfxksphaafkgrftpx9f.streamlit.app/"      # ✅ 이메일 인증 redirect용 (스트림릿 앱 주소로 교체)
 
 # ============================================================
 # ✅ App Settings
