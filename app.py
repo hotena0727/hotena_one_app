e67a4037bd3e6b414b00482e03ab935dea0c01c5
hotena0727 <kissitai0727@gmail.com>
2026-02-12 11:51:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1122,7 +1122,7 @@ def render_pronounce_button(text: str, uid: str, label: str = "🔊 발음"):
 }})();
 </script>
         """,
-        height=34,
+        height=43,
     )
 
 # ============================================================
